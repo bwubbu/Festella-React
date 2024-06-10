cbb09c7c879631edf879a651e2ab61e569928efe
kinderBono <116857873+nuraimandanial@users.noreply.github.com>
2024-06-11 04:04:19 +0800

    Update displaying bookmarked and registered events layout and styling

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,35 @@
-import React, { } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 import "../styles/Profile.css";
 import profilePicture from "../assets/pfpplaceholder.jpg";
 import { useAuth } from "../components/AuthContext";
+import { useEvent } from "../components/EventContext";
 
 function Profile() {
   const { user } = useAuth();
+  const { allEvents } = useEvent();
+  const [registeredEvents, setRegisteredEvents] = useState([]);
+  const [bookmarkedEvents, setBookmarkedEvents] = useState([]);
+
+  useEffect(() => {
+    if (user && user.profile) {
+      const fetchRegisteredEvents = () => {
+        const registeredEvents = allEvents.filter(event =>
+          user.profile.registeredEvents.includes(event._id)
+        );
+        setRegisteredEvents(registeredEvents);
+      }
+      const fetchBookmarkedEvents = () => {
+        const bookmarkedEvents = allEvents.filter(event =>
+          user.profile.bookmarkedEvents.includes(event._id)
+        );
+        setBookmarkedEvents(bookmarkedEvents);
+      }
+      fetchRegisteredEvents();
+      fetchBookmarkedEvents();
+    }
+  }, []);
 
   return (
     <div className="page-content">
@@ -38,34 +61,48 @@ function Profile() {
                 <h2 className="coloured-font">Registered</h2>
                 <h2>Events</h2>
               </div>
-              <div className="registered-events-container">
+              <div>
                 {user && user.profile && user.profile.registeredEvents && user.profile.registeredEvents.length > 0 ? (
-                  <div></div>
-                ): (
+                  <div className="registered-events-container">
+                    {registeredEvents.map(event => (
+                      <div className="event-card" key={event._id}>
+                        <img src={event.images[0]} alt={event.name} />
+                        <h3>{event.name}</h3>
+                      </div>
+                    ))}
+                  </div>
+                ) : (
                   <div>
                     <p>No events registered</p>
                   </div>
                 )}
+              </div>
+            </div>
+          </div>
+          <div className="bookmarked-events">
+            <div className="title">
+              <h2 className="coloured-font">Bookmarked</h2>
+              <h2>Events</h2>
+            </div>
+            <div>
+              {user && user.profile && user.profile.bookmarkedEvents && user.profile.bookmarkedEvents.length > 0 ? (
+                <div className="bookmarked-events-container">
+                  {bookmarkedEvents.map(event => (
+                    <div className="event-card" key={event._id}>
+                      <img src={event.images[0]} alt={event.name} />
+                      <h3>{event.name}</h3>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <div className="bookmarked-events-container">
+                  <p>No events bookmarked</p>
+                </div>
+              )}
             </div>
           </div>
         </div>
-        <div className="bookmarked-events">
-          <div className="title">
-            <h2 className="coloured-font">Bookmarked</h2>
-            <h2>Events</h2>
-          </div>
-          <div className="bookmarked-events-container">
-            {user && user.profile && user.profile.bookmarkedEvents && user.profile.bookmarkedEvents.length > 0 ? (
-              <div></div>
-            ) : (
-              <div>
-                <p>No events bookmarked</p>
-              </div>
-            )}
-          </div>
-        </div>
       </div>
-    </div>
     </div >
   );
 }
