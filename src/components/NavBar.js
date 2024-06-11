13f6f541f60f53ca95075ad9455827ca60d517da
kinderBono <116857873+nuraimandanial@users.noreply.github.com>
2024-06-11 13:59:53 +0800

    Added Event filter search functions

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../assets/logo.png';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import '../styles/NavBar.css';
 import ReorderIcon from '@mui/icons-material/Reorder';
 import { useAuth } from './AuthContext';
+import { useEvent } from './EventContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function NavBar() {
   const { isAuthenticated, user, logout } = useAuth();
@@ -23,14 +26,38 @@ function NavBar() {
     logout();
   }
 
+  const [searchInput, setSearchInput] = useState('');
+  const { allEvents } = useEvent();
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    setSearchResults(allEvents.filter(event =>
+      event.name.toLowerCase().includes(searchInput.toLowerCase())
+    ))
+  }, [searchInput]);
+
+  const handleSearchChange = (e) => {
+    setSearchInput(e.target.value);
+  }
+
   return (
     <div className='navbar'>
       <div className='leftSide' id={openLinks ? "open" : "close"}>
-        <img src={logo} alt='logo' />
+        <NavLink to='/'><img src={logo} alt='logo' /></NavLink>
         <form id='search' action='#'>
-          <input type='text' placeholder='Type Something' id='searchText' name='searchKeyword' />
-          <i></i>
+          <input type='text' placeholder='Type Something' id='searchText' name='searchKeyword' value={searchInput} onChange={handleSearchChange} />
+          <FontAwesomeIcon className='search-icon' icon={faSearch} />
         </form>
+        {searchInput !== '' && searchResults.length > 0 && (
+          <div className='event-list-results'>
+            {searchResults.map(event => (
+              <NavLink className='event-result-item' key={event._id} to='/browse/eventdetails' state={{ eventId: event._id }} onClick={() => setSearchInput('')} >
+                <img src={event.images[0]} alt={event.name} />
+                {event.name}
+              </NavLink>
+            ))}
+          </div>
+        )}
         <div className='hiddenLinks'>
           <NavLink exact="true" to="/" activeclassname="active">Home</NavLink>
           <NavLink to='/browse' activeclassname="active">Browse</NavLink>
